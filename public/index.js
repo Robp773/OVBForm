1c42c598b8a5a1dc004098bb3b5629cccbad8755
Robp773 <robpeterman573@gmail.com>
2018-03-20 21:06:41 -0400

    basic html layout with jQuery

---
+++ public/index.js
@@ -0,0 +1,14 @@
+$('#medicalReasonInfo').hide()
+$('#applicationForm').submit(function(e) {
+	e.preventDefault()
+	let value = $('.primary').val()
+	window.print()
+})
+$('#medicalTrue').change(function(e) {
+	if ($('#medicalTrue').is(':checked')) {
+		$('#medicalReasonInfo').show()
+	}
+})
+$('#medicalFalse').change(function(e) {
+	$('#medicalReasonInfo').hide()
+})
